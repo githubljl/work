8e82050b06012b7cb4e0d45ec2e8958d6e562165
lijinglun <281965992@qq.com>
2016-01-07 18:00:01 +0800

    更新内容

--- js/my.js
+++ js/my.js
@@ -2,7 +2,18 @@
  * Created by Administrator on 2016/1/5.
  */
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/404', {
+        templateUrl: '404.html',
+    }).when('/back', {
+        templateUrl: 'babyback.html',
+    }).otherwise({redirectTo: '/404'});
+    //$urlRouterProvider.when("", "404");
+    //$urlRouterProvider.otherwise('/404');
+    //$urlRouterProvider.html5Mode(true);
+}]);
+
 app.controller('myCtrl', ['$scope' , '$http', function($scope, $http) {
     var url='js/json.js';
     $http.get(url)
@@ -27,26 +38,7 @@ app.controller('myCtrl', ['$scope' , '$http', function($scope, $http) {
             console.log(data);
         })
 
-}]);
+}
+]);
 
-//babyApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-//    var url = 'http://each.sinaapp.com/wechat/url2text.php?http://qzonestyle.gtimg.cn/qzone_v6/lostchild/data.js';
-//    $http({method: 'GET', url: url}).then(function(res) {
-//        if (res.data) {
-//            var data = res.data;
-//            data = data.substring(data.indexOf('{'), data.lastIndexOf('}') + 1);
-//            data = JSON.parse(data);
-//            $scope.children = data.data;
-//            $scope.imgHost = 'http://qzone.qq.com/gy/upload/';
-//            // $scope.defaultImg = 'http://tp3.sinaimg.cn/1933895942/180/5662863775/1';
-//        }
-//    }, function(res) {
-//        console.log(res);
-//    });
-//
-//    $scope.defaultBabyImg = defaultBabyImg;
-//    $scope.currentChild = null;
-//    $scope.getCurrentChild = function(child) {
-//        $scope.currentChild = child;
-//    };
-//}]);
+
